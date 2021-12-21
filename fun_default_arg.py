8c193994809817e430e243e56e1b26aee24aeb37
ranjitkumarpatnaik <mr.ranjitpatnaik@gmail.com>
2021-12-21 11:34:40 +0530

    added few more examples

--- fun_default_arg.py
+++ fun_default_arg.py
@@ -25,3 +25,42 @@ def working_on(user="root"):
     print(f"\nLogged in as: {user}")
     return None
 working_on("admin") #specify user as admin,spiderman,ironman to check
+
+#Functions with keyword based arguments
+
+def user_arg(a,b):
+    print(f"a= {a}")
+    return None
+
+user_arg(4,5) #a=4,b=5
+user_arg(b=5,a=4) #We want a=4,b=5 always even when we change the positions
+
+
+
+#Functions with variable length arguments:
+
+def arg_var(*data): # adding * will make the code to accept multiple arguments
+    for each in data:
+        print(type(each))
+    return None
+arg_var(4)
+arg_var(5,5)
+arg_var("hi",4.5,7)
+
+#functions with variable length keyword-based arguments
+def key_arg(**karg):
+    print("\n",karg)
+    return None
+
+key_arg(a=4,b=5)
+key_arg(a=7,b=8,c=9)
+key_arg(x=2,y="Hi",z=6.7,user="root") #inside the function we are getting the passed variable as a dictionary
+
+
+
+
+
+
+
+
+
