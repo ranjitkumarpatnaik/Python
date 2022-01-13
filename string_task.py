67fd68d217eadd8f257b9f63b5635baedc6ce868
ranjitkumarpatnaik <mr.ranjitpatnaik@gmail.com>
2022-01-13 12:04:19 +0530

    removed files

--- string_task.py
+++
@@ -1,5 +0,0 @@
-#Display given string at left/right/center of a line in a title format
-import os
-width=os.get_terminal_size().columns
-given_str= input("Enter your string:  ")
-print(f'\n{given_str.ljust(width).title()} \n{given_str.center(width).title()} \n{given_str.rjust(width).title()}')
